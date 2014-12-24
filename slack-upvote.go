ba8eae135210ed61015c46710c25af8c32173ead
John Adams <johnadams@jqatampa.com>
2014-12-24 18:41:57 +0000

    slack formatting...trying attachments

--- slack-upvote.go
+++ slack-upvote.go
@@ -94,9 +94,9 @@ func VoteHandler(rw http.ResponseWriter, r *http.Request) {
 		var m Mention
 		for iter.Next(&m) {
 			sb.WriteString(fmt.Sprintf("[*'%s'* has a score of *%v*], ", m.Id, m.Votes))
-			fb.WriteString(fmt.Sprintf("*'%s'* has a score of *%v*\n", m.Id, m.Votes))
+			fb.WriteString(fmt.Sprintf("*'%s'* has a score of *%v*", m.Id, m.Votes))
 		}
-		rw.Write([]byte(fmt.Sprintf("{\"text\":\"%s\", \"mkdown\":true, \"attachments\":[{\"fallback\":\"%s\", \"fields\" : [{ \"title\":\"%s\",\"value\":\"%s\"}]}]}", sb.String(), sb.String(), title, fb.String())))
+		rw.Write([]byte(fmt.Sprintf("{\"text\":\"%s\", \"mkdown\":true, \"attachments\":[{\"fallback\":\"%s\", \"fields\" : [{ \"title\":\"%s\", \"value\":\"%s\"}] }] }", sb.String(), sb.String(), title, fb.String())))
 	}
 }
 func main() {
